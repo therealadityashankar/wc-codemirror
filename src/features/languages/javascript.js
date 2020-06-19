72bf3a6d738843feec10a0000c5d50886d930b5b
Aditya Shankar <me@adityashankar.xyz>
2020-06-19 22:30:45 +0530

    added code for runtime javascript

---
+++ src/features/languages/javascript.js
@@ -0,0 +1,10 @@
+// we're running javascript in javascript
+// nothing special is required
+
+window.WCCodeMirrorExtras.languages.javascript = {
+	abilities:{
+		run(){
+			console.log("running javascript code");
+		}
+	}
+}
